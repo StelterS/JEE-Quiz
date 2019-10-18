4ca9aab66ccc1c9b86591170c9525c8f28a54995
Stelter Szymon <33425406+StelterS@users.noreply.github.com>
2019-10-18 14:18:32 +0200

    REST API for Exercises and Answers

---
+++ src/main/java/com/klopsi/answer/resource/AnswerResource.java
@@ -0,0 +1,75 @@
+package com.klopsi.answer.resource;
+
+import com.klopsi.answer.AnswerService;
+import com.klopsi.answer.model.Answer;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.util.Collection;
+
+@Path("answers")
+public class AnswerResource {
+
+	@Inject
+	private AnswerService answerService;
+
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<Answer> getAllAnswers() {
+		return answerService.findAllAnswers();
+	}
+
+	@GET
+	@Path("{answerId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAnswer(@PathParam("answerId") int answerId) {
+		Answer answer = answerService.findAnswer(answerId);
+		if (answer != null) {
+			return Response.ok(answer).build();
+		} else {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response saveAnswer(Answer answer) {
+		answerService.saveAnswer(answer);
+		return Response
+			.created(UriBuilder.fromResource(AnswerResource.class).path(AnswerResource.class, "getAnswer").build(answer.getId()))
+			.build();
+	}
+
+	@PUT
+	@Path("{answerId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateAnswer(@PathParam("answerId") int answerId, Answer answer) {
+		Answer originalAnswer = answerService.findAnswer(answerId);
+		if (originalAnswer == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		} else if (originalAnswer.getId() != answer.getId()) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		} else {
+			answerService.deleteAnswerFromExerciseList(originalAnswer);
+			answerService.saveAnswer(answer);
+			return Response.ok().build();
+		}
+	}
+
+	@DELETE
+	@Path("{answerId}")
+	public Response deleteAnswer(@PathParam("answerId") int answerId) {
+		Answer answer = answerService.findAnswer(answerId);
+		if (answer == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		} else {
+			answerService.removeAnswer(answer);
+			return Response.noContent().build();
+		}
+	}
+
+}
---
+++ src/main/java/com/klopsi/exercise/resource/ExerciseResource.java
@@ -0,0 +1,106 @@
+package com.klopsi.exercise.resource;
+
+import com.klopsi.exercise.ExerciseService;
+import com.klopsi.exercise.model.Exercise;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.util.Collection;
+
+@Path("/exercises")
+public class ExerciseResource {
+
+	@Inject
+	private ExerciseService exerciseService;
+
+	/**
+	 * Fetch all exercises available.
+	 *
+	 * @return all exercises
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<Exercise> getAllExercises(){
+		System.out.println("Get them all buahahaha!");
+		return exerciseService.findAllExercises();
+	}
+
+
+	/**
+	 * Find single book
+	 *
+	 * @param exerciseId exercise id
+	 * @return response with exercise entity or 404 code
+	 */
+	@GET
+	@Path("{exerciseId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getExercise(@PathParam("exerciseId") int exerciseId){
+		Exercise exercise = exerciseService.findExercise(exerciseId);
+		if (exercise != null) {
+			return Response.ok(exercise).build();
+		}
+		else {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+	}
+
+	/**
+	 * Saves new exercise
+	 * @param exercise new exercise
+	 * @return response with 201 code and new object url
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response saveExercise(Exercise exercise) {
+		exerciseService.saveExercise(exercise);
+		return Response
+			.created(UriBuilder.fromResource(ExerciseResource.class).path(ExerciseResource.class, "getExercise").build(exercise.getId()))
+			.build();
+	}
+
+	/**
+	 * updates single exercise
+	 * @param exerciseId exercise id
+	 * @param exercise exercise edited
+	 * @return response 200 code or 404 when exercise does not exist or 400 when exercise ids mismatch
+	 */
+	@PUT
+	@Path("{exerciseId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateExercise(@PathParam("exerciseId") int exerciseId, Exercise exercise) {
+		Exercise originalExercise = exerciseService.findExercise(exerciseId);
+		if (originalExercise == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		else if (originalExercise.getId() != exercise.getId()) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+		else {
+			exercise.setAnswers(originalExercise.getAnswers());
+			exerciseService.saveExercise(exercise);
+			return Response.ok().build();
+		}
+	}
+
+	/**
+	 * Delete single exercise
+	 * @param exerciseId exercise id
+	 * @return response 204 code or 404 when exercise with given id does not exist
+	 */
+	@DELETE
+	@Path("{exerciseId}")
+	public Response deleteExercise(@PathParam("exerciseId") int exerciseId) {
+		Exercise exercise = exerciseService.findExercise(exerciseId);
+		if (exercise == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		else {
+			exerciseService.removeExercise(exercise);
+			return Response.noContent().build();
+		}
+	}
+}
