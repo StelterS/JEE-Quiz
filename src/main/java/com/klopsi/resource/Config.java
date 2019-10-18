6efbb30193cc4c4317b71743bf657f24fe41f453
Stelter Szymon <33425406+StelterS@users.noreply.github.com>
2019-10-18 14:17:51 +0200

    Config for REST API

---
+++ src/main/java/com/klopsi/resource/Config.java
@@ -0,0 +1,12 @@
+package com.klopsi.resource;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/*
+* Global REST prefix configuration
+* */
+
+@ApplicationPath("/api")
+public class Config extends Application {
+}
