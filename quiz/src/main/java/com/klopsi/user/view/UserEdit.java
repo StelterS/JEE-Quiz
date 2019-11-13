ad564f83528d8417bce98ecf50d5b7a0f22dcb63
Stelter Szymon <33425406+StelterS@users.noreply.github.com>
2019-11-13 13:28:20 +0100

    User roles can be changed by admins

--- quiz/src/main/java/com/klopsi/user/view/UserEdit.java
+++ quiz/src/main/java/com/klopsi/user/view/UserEdit.java
@@ -2,6 +2,7 @@ package com.klopsi.user.view;
 
 import com.klopsi.user.UserService;
 import com.klopsi.user.model.User;
+import lombok.Getter;
 import lombok.Setter;
 
 import javax.faces.view.ViewScoped;
@@ -16,12 +17,29 @@ import java.util.List;
 public class UserEdit implements Serializable {
     private UserService service;
 
+    @Getter
+    @Setter
+    private String newUserRole;
+
     @Setter
     private User user;
 
     public User getUser() {
         if(user == null) {
             user = new User();
+        }
+        List<String> roles = user.getRoles();
+        if(roles != null) {
+            // check what role does user have
+            if(roles.contains(User.Roles.ADMIN)){
+                newUserRole = User.Roles.ADMIN;
+            }
+            else if (roles.contains(User.Roles.MODERATOR)){
+                newUserRole = User.Roles.MODERATOR;
+            }
+            else {
+                newUserRole = User.Roles.USER;
+            }
         }
         return user;
     }
@@ -41,6 +59,16 @@ public class UserEdit implements Serializable {
     }
 
     public String saveUser(){
+        // call service to change user roles
+        if(newUserRole.equals(User.Roles.ADMIN)) {
+            user.setRoles(List.of(User.Roles.USER, User.Roles.MODERATOR, User.Roles.ADMIN));
+        }
+        else if(newUserRole.equals(User.Roles.MODERATOR)) {
+            user.setRoles(List.of(User.Roles.USER, User.Roles.MODERATOR));
+        }
+        else {
+            user.setRoles(List.of(User.Roles.USER));
+        }
         service.saveUser(user);
         return "user_list?faces-redirect=true";
     }
